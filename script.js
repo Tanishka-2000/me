8a3110451396e9b252372e3198b510160bdaeab0
Shashank Talwar <shashanktalwar81@gmail.com>
2022-09-26 14:05:16 +0530

    change styles

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ let projects = document.querySelectorAll('section.project');
 // console.log(projects[0].getAttribute('href'));
 navLinks.forEach(link => {
     link.addEventListener('click', showProject);
+    link.addEventListener('focus', stopScrolling, {passive: false});
 });
 function showProject(e){
     let temp = e.target.getAttribute('href');
     projects.forEach(project => project.classList.remove('show'));
     document.querySelector(temp).classList.add('show');
+}
+function stopScrolling(e){
+    console.log(e);
+    e.preventDefault();
 }
