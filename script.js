585d10125450bfe73572e5eaf11e9165d6d85bf2
Shashank Talwar <shashanktalwar81@gmail.com>
2022-09-26 22:00:56 +0530

    add close btn to section

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 let navLinks = document.querySelectorAll('nav a');
 let projects = document.querySelectorAll('section.projects');
-// console.log(projects[0].getAttribute('href'));
+let projCloseBtn = document.querySelectorAll('.projects button');
+
 navLinks.forEach(link => {
     link.addEventListener('click', showProject);
+    link.addEventListener('mousedown', showProject);
 });
 function showProject(e){
     let temp = e.target.getAttribute('href');
     projects.forEach(project => project.classList.remove('show'));
-    // document.querySelector(temp).style.display = 'block';
     document.querySelector(temp).classList.add('show');
+}
+projCloseBtn.forEach(btn => btn.addEventListener('click', hideProject));
+function hideProject(e){
+    console.log(e.target.parentElement);
+    e.target.parentElement.classList.remove('show');
 }
