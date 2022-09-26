e1d54979efecd381ccb16c89301fc5ae20889a56
Shashank Talwar <shashanktalwar81@gmail.com>
2022-09-26 17:18:08 +0530

    add project sections

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 let navLinks = document.querySelectorAll('nav a');
-let projects = document.querySelectorAll('section.project');
+let projects = document.querySelectorAll('section.projects');
 // console.log(projects[0].getAttribute('href'));
 navLinks.forEach(link => {
     link.addEventListener('click', showProject);
-    link.addEventListener('focus', stopScrolling, {passive: false});
 });
 function showProject(e){
     let temp = e.target.getAttribute('href');
     projects.forEach(project => project.classList.remove('show'));
+    // document.querySelector(temp).style.display = 'block';
     document.querySelector(temp).classList.add('show');
-}
-function stopScrolling(e){
-    console.log(e);
-    e.preventDefault();
 }
