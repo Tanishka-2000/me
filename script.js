c2669a4c3ce2750050d9ea3be7cfac4d3799bf7d
Shashank Talwar <shashanktalwar81@gmail.com>
2022-09-27 23:10:29 +0530

    add event listener to nav div

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-let navLinks = document.querySelectorAll('nav a');
+// let navLinks = document.querySelectorAll('nav a');
+let navLinks  = document.querySelectorAll('nav div');
 let projects = document.querySelectorAll('section.projects');
 let projCloseBtn = document.querySelectorAll('.projects button');
 
-navLinks.forEach(link => {
-    link.addEventListener('click', showProject);
-    link.addEventListener('mousedown', showProject);
-});
+// navLinks.forEach(link => {
+//     link.addEventListener('click', showProject);
+//     link.addEventListener('mousedown', showProject);
+// });
+
+function hideProject(e){
+    e.target.parentElement.classList.remove('show');
+}
+
 function showProject(e){
-    let temp = e.target.getAttribute('href');
+    let temp = e.target;
+
+    if(temp.localName == 'div') temp = temp.firstElementChild;
+    else if(temp.localName == 'span') temp = temp.parentElement;
+
+    temp = temp.getAttribute('href');
     projects.forEach(project => project.classList.remove('show'));
     document.querySelector(temp).classList.add('show');
 }
+
 projCloseBtn.forEach(btn => btn.addEventListener('click', hideProject));
-function hideProject(e){
-    console.log(e.target.parentElement);
-    e.target.parentElement.classList.remove('show');
-}
+
+navLinks.forEach(div => {
+    div.addEventListener('click', showProject);
+});
