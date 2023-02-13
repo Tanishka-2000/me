f650d49643ca0684c5275da904f9dbb6e1ed0f49
Shashank Talwar <shashanktalwar81@gmail.com>
2023-02-13 17:04:40 +0530

    show divs on click

--- script.js
+++ script.js
@@ -1,30 +1,13 @@
-// let navLinks = document.querySelectorAll('nav a');
-let navLinks  = document.querySelectorAll('nav div');
-let projects = document.querySelectorAll('section.projects');
-let projCloseBtn = document.querySelectorAll('.projects button');
+let prev = null;
 
-// navLinks.forEach(link => {
-//     link.addEventListener('click', showProject);
-//     link.addEventListener('mousedown', showProject);
-// });
-
-function hideProject(e){
-    e.target.parentElement.classList.remove('show');
+function hideDiv(e){
+    prev.classList.remove('show');
+    prev = null;
 }
 
-function showProject(e){
-    let temp = e.target;
-
-    if(temp.localName == 'div') temp = temp.firstElementChild;
-    else if(temp.localName == 'span') temp = temp.parentElement;
-
-    temp = temp.getAttribute('href');
-    projects.forEach(project => project.classList.remove('show'));
-    document.querySelector(temp).classList.add('show');
+function showDiv(curr){
+    if(prev) prev.classList.remove('show');
+    curr.classList.add('show');
+    prev = curr;
 }
 
-projCloseBtn.forEach(btn => btn.addEventListener('click', hideProject));
-
-navLinks.forEach(div => {
-    div.addEventListener('click', showProject);
-});
